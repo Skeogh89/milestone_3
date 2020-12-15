4ad9484d6bffbd9e93f0c8181c9563a2885bc4e9
Stephen Keogh <stevokeogh@hotmail.com>
2020-12-15 18:46:58 +0000

    added delte operation

--- app.py
+++ app.py
@@ -157,6 +157,13 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipes=recipes, cuisines=cuisines, prep_times=prep_times, total_times=total_times, yeilds=yields)
 
 
+@app.route("/delete_task/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Recipe Successfully Deleted!")
+    return redirect(url_for("get_recipes"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
